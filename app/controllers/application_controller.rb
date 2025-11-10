746c716de001d32bbae75e8f5c16ddc3a7bb9016
Ubuntu <ubuntu@ip-172-31-21-198.ap-northeast-1.compute.internal>
2025-11-10 02:11:35 +0000

    [ログイン認証機能の実装]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # 新規登録時
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    # ログイン時
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+  end
+
 end
