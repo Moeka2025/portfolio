763d9a9678d383c2bf77f57b069c087a0fa7f3e0
Ubuntu <ubuntu@ip-172-31-21-198.ap-northeast-1.compute.internal>
2025-11-14 01:23:31 +0000

    [表示漏れの修正、画像表示の条件分岐、一覧の並び順、ログインユーザーのみ 詳細・編集・更新・削除ボタンを表示する]

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,6 @@
 class SpotsController < ApplicationController
+  before_action :set_spot, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
   
   def new
     @spot = Spot.new
@@ -8,7 +10,6 @@ class SpotsController < ApplicationController
   def create
     @spot = Spot.new(spot_params)
     @spot.user_id = current_user.id # ユーザーと紐づける
-
     if @spot.save
       redirect_to spot_path(@spot), notice: '投稿が保存されました'
     else
@@ -18,7 +19,7 @@ class SpotsController < ApplicationController
 
 
   def index
-    @spot_images = Spot.all
+    @spots = Spot.order(created_at: :desc)
   end
 
   def show
@@ -29,6 +30,22 @@ class SpotsController < ApplicationController
     @spot = Spot.find(params[:id])
   end
 
+  def update
+    @spot = Spot.find(params[:id])
+    if @spot.update(spot_params)
+      redirect_to spot_path(@spot), notice: "更新しました"
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @spot = Spot.find(params[:id])
+    @spot.destroy
+    redirect_to spots_path, notice: '削除しました'
+  end
+
 
   private
 
@@ -36,4 +53,13 @@ class SpotsController < ApplicationController
     params.require(:spot).permit(:title, :body, :image)
   end
 
+  def set_spot
+    @spot =Spot.find(params[:id])
+  end
+
+  def authorize_user!
+    unless @spot.user == current_user
+      redirect_to  spots_path, alert: "権限がありません"
+    end
+  end
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,13 +1,16 @@
 class Spot < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
   has_one_attached :image
 
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
+
   def get_image
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/いらすとや_no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    if image.attached?
+      image
+    else
+      'いらすとや_no_image.jpg'
     end
-    image
   end
 end
