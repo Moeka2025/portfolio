98024e86ff953a48e1c56ec0ea41972f20488c2c
Ubuntu <ubuntu@ip-172-31-21-198.ap-northeast-1.compute.internal>
2026-01-06 12:38:11 +0000

    [Fix spot_tags migration]

--- db/migrate/20251223094736_add_correct_columns_to_spot_tags.rb
+++ db/migrate/20251223094736_add_correct_columns_to_spot_tags.rb
@@ -1,6 +1,11 @@
 class AddCorrectColumnsToSpotTags < ActiveRecord::Migration[6.1]
   def change
-    add_column :spot_tags, :spot_id, :integer
-    add_column :spot_tags, :tag_id, :integer
+    unless column_exists?(:spot_tags, :spot_id)
+      add_column :spot_tags, :spot_id, :integer
+    end
+
+    unless column_exists?(:spot_tags, :tag_id)
+      add_column :spot_tags, :tag_id, :integer
+    end
   end
 end
